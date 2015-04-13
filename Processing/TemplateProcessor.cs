2ee5552ef5747cc45058c0913a66b0c8ae1d8bf1
Evgeniy Plyusnin <eninbox@gmail.com>
2015-04-13 15:17:06 +0500

    Захреначено регулярное выражение для парсинга параметров

--- Processing/TemplateProcessor.cs
+++ Processing/TemplateProcessor.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace QmlObjectPropertiesCodeGenerator.Processing
 {
@@ -10,6 +12,18 @@ namespace QmlObjectPropertiesCodeGenerator.Processing
         /// <summary>Обрабатывает шаблон и строит блок кода на его основе</summary>
         /// <param name="Template">Шаблон генерируемого кода</param>
         /// <param name="Properties">Словарь свойств для генерации кода</param>
-        public string ProcessTemplate(string Template, IDictionary<string, string> Properties) { }
+        public string ProcessTemplate(string Template, IDictionary<string, string> Properties)
+        {
+            Regex regex = new Regex(@"\{(?<name>\w+)(:((?<extension>[\w-]+)\s?)+)?\}");
+            var matches = regex.Matches(Template);
+
+            foreach (var match in matches.OfType<Match>())
+            {
+                var name = match.Groups["name"].Value;
+                var extensions = match.Groups["extension"].Captures.OfType<Capture>().Select(c => c.Value).ToList();
+            }
+
+            return null;
+        }
     }
 }
